f6100ef6ad82e0b8dfd4bcb619f04416a48c3178
kenan <xinkenan.11@hotmail.com>
2022-11-17 13:22:41 +0800

    added working build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	define: {
+		"process.env": {},
+	},
+	build: {
+		// cssCodeSplit: false,
+		emptyOutDir: false,
+		lib: {
+			entry: resolve(__dirname, "src/index.tsx"),
+			name: "Widget",
+			fileName: "widget",
+			formats: ["es", "umd"],
+		},
+	},
+});
